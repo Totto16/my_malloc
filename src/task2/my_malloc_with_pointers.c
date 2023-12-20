339e6dc094f4ae4b9f38f954f6ba005515e4123d
Totto16 <tobiausgais@gmail.com>
2023-12-20 17:11:01 +0100

    -fix nasty bug in my_malloc with double pointers - forgot to adjust a pointer in adding a new block - remove __my_malloc_fill_status, since it's not that useful - remove all debugging things in the double pointer example - make macro SIZE_OF_DOUBLE_POINTER_BLOCK into function - don't have a mutex in the global struct, when using _PER_THREAD_ALLOCATORm alias thread_local storage - uncommented atexit, since it has to be investigated, when used with multiple threads!

--- src/task2/my_malloc_with_pointers.c
+++ src/task2/my_malloc_with_pointers.c
@@ -17,15 +17,6 @@ Module: PS OS 10
 
 #include "my_malloc.h"
 
-#if !defined(_VALIDATE_BLOCKS)
-#define _VALIDATE_BLOCKS 0
-#endif
-
-#if _VALIDATE_BLOCKS < 0 || _VALIDATE_BLOCKS > 1
-// this is a c preprocessor macro, it throws a compiler error with the given message
-#error "NOT SUPPORTED VALIDATE_BLOCKS: not between 0 and 1!"
-#endif
-
 #if !defined(_PER_THREAD_ALLOCATOR)
 #define _PER_THREAD_ALLOCATOR 0
 #endif
@@ -39,6 +30,8 @@ Module: PS OS 10
 // that it doesn't pollute the global scope additionally these are made static! (meaning no outside
 // file can see them, on global variables this only makes them inivisible to other files )
 
+typedef uint8_t status_t;
+
 typedef uint8_t pseudoByte;
 
 // FREE is 0, so each mmap (so initialized to 0) region has set every block to FREE
@@ -46,12 +39,6 @@ enum __my_malloc_alloc_status {
 	FREE = 0,
 	ALLOCED = 1,
 };
-enum __my_malloc_fill_status { FULLY_FILLED = 0, NOT_FULLY_FILLED = 1 };
-
-typedef struct {
-	uint8_t alloc_state : 1;
-	uint8_t fill_state : 1;
-} status_t;
 
 typedef struct {
 	void* nextBlock;
@@ -59,26 +46,10 @@ typedef struct {
 	status_t status;
 } BlockInformation;
 
-#define SIZE_OF_DOUBLE_POINTER_BLOCK(variableName, block) \
-	uint64_t variableName; \
-	do { \
-		if((block) == NULL) { \
-			printSingleErrorAndExit( \
-			    "INTERNAL: This is an allocator ERROR, this shouldn't occur!\n"); \
-		} else if((block)->nextBlock == NULL) { \
-			(variableName) = (pseudoByte*)__my_malloc_globalObject.data + \
-			                 __my_malloc_globalObject.dataSize - sizeof(BlockInformation) - \
-			                 (pseudoByte*)(block); \
-		} else { \
-			(variableName) = ((pseudoByte*)(block)->nextBlock) - (pseudoByte*)(block) - \
-			                 sizeof(BlockInformation); \
-		} \
-	} while(false)
-
 typedef struct {
 	void* data;
 	uint64_t dataSize;
-#ifndef _ALLOCATOR_NOT_MT_SAVE
+#if !defined(_ALLOCATOR_NOT_MT_SAVE) && _PER_THREAD_ALLOCATOR != 1
 	pthread_mutex_t mutex;
 #endif
 } GlobalObject;
@@ -96,89 +67,30 @@ static GlobalObject __my_malloc_globalObject = { .data = NULL };
 static _Thread_local GlobalObject __my_malloc_globalObject = { .data = NULL };
 #endif
 
-#if _VALIDATE_BLOCKS == 1
-// could be a macro, but for more complicated use later thats not that well suited
-static bool __my_malloc_isValidBlock(void* blockPointer) {
-	if(blockPointer == NULL) {
-		return false;
-	}
-
-	BlockInformation* blockInformation = ((BlockInformation*)blockPointer);
-	bool hasValidStatus = ((blockInformation->status.alloc_state) == FREE ||
-	                       (blockInformation->status.alloc_state) == ALLOCED) &&
-	                      ((blockInformation->status.fill_state) == FULLY_FILLED ||
-	                       (blockInformation->status.fill_state) == NOT_FULLY_FILLED);
-
-	bool nextIsInRange =
-	    ((blockInformation->nextBlock == NULL ||
-	      (blockInformation->nextBlock >= __my_malloc_globalObject.data &&
-	       (pseudoByte*)blockInformation->nextBlock <=
-	           (pseudoByte*)__my_malloc_globalObject.data + __my_malloc_globalObject.dataSize)) &&
-	     (blockInformation->previousBlock == NULL ||
-	      (blockInformation->previousBlock >= __my_malloc_globalObject.data &&
-	       (pseudoByte*)blockInformation->previousBlock <=
-	           (pseudoByte*)__my_malloc_globalObject.data + __my_malloc_globalObject.dataSize)));
-
-	bool noSelfReference =
-	    (blockInformation->nextBlock == NULL || blockInformation->nextBlock != blockInformation) &&
-	    (blockInformation->previousBlock == NULL ||
-	     blockInformation->previousBlock != blockInformation);
-
-	SIZE_OF_DOUBLE_POINTER_BLOCK(blockSize, blockInformation);
-
-	bool blockSizeIsInBoundaries =
-	    blockSize <=
-	    __my_malloc_globalObject.dataSize; // <= 0 on unsigned is not necessary, it underflows then!
-
-#if _PRINT_DEBUG == 1
-	if(!(nextIsInRange && hasValidStatus && noSelfReference && blockSizeIsInBoundaries)) {
-		printf(
-		    "nextIsInRange: %d, hasValidStatus: %d, noSelfReference: %d, blockSizeIsInBoundaries: "
-		    "%d\n",
-		    nextIsInRange, hasValidStatus, noSelfReference, blockSizeIsInBoundaries);
-	}
-#endif
-
-	return nextIsInRange && hasValidStatus && noSelfReference && blockSizeIsInBoundaries;
-}
-
-#endif
-
-// DEBUG
-#if _PRINT_DEBUG == 1
-static void __my_malloc_debug_printSegment(char* desc, BlockInformation* information);
-
-static void __my_malloc_debug_printSegment(char* desc, BlockInformation* information) {
-#if _VALIDATE_BLOCKS == 1
-	if(information != NULL && !__my_malloc_isValidBlock(information)) {
-		printSingleErrorAndExit("NOT VALID: in print\n");
-	}
-#endif
-
-	SIZE_OF_DOUBLE_POINTER_BLOCK(blockSize, information);
-	printf("%s%sBlock: |%c|%c| %ld -> %s\n", desc != NULL ? "\n" : "\t", desc != NULL ? desc : "",
-	       (information->status.alloc_state) == FREE ? ' ' : '#',
-	       (information->status.fill_state) == FULLY_FILLED ? 'F' : ' ', blockSize,
-	       information->nextBlock == NULL ? "END\n" : "next");
-	if(information->nextBlock != NULL) {
-		__my_malloc_debug_printSegment(NULL, information->nextBlock);
+uint64_t size_of_double_pointer_block(BlockInformation* block) {
+	if(block == NULL) {
+		printSingleErrorAndExit("INTERNAL: This is an allocator ERROR, this shouldn't occur!\n");
+	} else if(block->nextBlock == NULL) {
+		return (((pseudoByte*)__my_malloc_globalObject.data + __my_malloc_globalObject.dataSize) -
+		        (pseudoByte*)block) -
+		       sizeof(BlockInformation);
+	} else {
+		return ((pseudoByte*)block->nextBlock - (pseudoByte*)block) - sizeof(BlockInformation);
 	}
 }
-#endif
-// DEBUG
 
 static bool __my_malloc_block_fitsBetter(BlockInformation* toCompare,
                                          BlockInformation* currentBlock, uint64_t size) {
 
-	if(toCompare->status.alloc_state != FREE) {
+	if(toCompare->status != FREE) {
 		return false;
 	}
 
-	if(currentBlock->status.alloc_state != FREE) {
+	if(currentBlock->status != FREE) {
 		return true;
 	}
 
-	SIZE_OF_DOUBLE_POINTER_BLOCK(blockSize, toCompare);
+	const uint64_t blockSize = size_of_double_pointer_block(toCompare);
 
 	// if a new block has to be "allocated" then there has to be space for that!
 	if(toCompare->nextBlock == NULL) {
@@ -204,7 +116,7 @@ static bool __my_malloc_block_fitsBetter(BlockInformation* toCompare,
 		return true;
 	}
 
-	SIZE_OF_DOUBLE_POINTER_BLOCK(currentSize, currentBlock);
+	const uint64_t currentSize = size_of_double_pointer_block(currentBlock);
 
 	if(currentSize > size + sizeof(BlockInformation)) {
 		if(blockSize <= size + sizeof(BlockInformation)) {
@@ -224,7 +136,8 @@ void* my_malloc(uint64_t size) {
 		fprintf(stderr, "Calling malloc before initializing the allocator is prohibited!\n");
 		exit(1);
 	}
-#ifndef _ALLOCATOR_NOT_MT_SAVE
+
+#if !defined(_ALLOCATOR_NOT_MT_SAVE) && _PER_THREAD_ALLOCATOR != 1
 	int result = pthread_mutex_lock(&__my_malloc_globalObject.mutex);
 	// mutex errors are better when being asserted, since no real errors can occur, only when the
 	// system is already malfunctioning
@@ -239,34 +152,35 @@ void* my_malloc(uint64_t size) {
 		// TODO: this is extremely slow, this WIP tries to make some cases faster!!
 
 		/*
-		        SIZE_OF_DOUBLE_POINTER_BLOCK(blockSize, bestFit);
+		        size_of_double_pointer_block(blockSize, bestFit);
 		        if(blockSize + sizeof(BlockInformation) <= size) {
 		            break;
 		        }
 
-		        SIZE_OF_DOUBLE_POINTER_BLOCK(nextSize, nextFreeBlock);
+		        size_of_double_pointer_block(nextSize, nextFreeBlock);
 		        if(nextSize + sizeof(BlockInformation) <= size) {
 		            bestFit = nextFreeBlock;
 		        } */
 
 		if(__my_malloc_block_fitsBetter(nextFreeBlock, bestFit, size)) {
 			bestFit = nextFreeBlock;
-			// shorthand evaluation, so if it fits perfectly don't look fort better
-			SIZE_OF_DOUBLE_POINTER_BLOCK(blockSize, bestFit);
+			// shorthand evaluation, so if it fits perfectly don't look for a better one
+			const uint64_t blockSize = size_of_double_pointer_block(bestFit);
 			if(blockSize == size) {
 				break;
 			}
 		}
 		nextFreeBlock = nextFreeBlock->nextBlock;
 	}
-	// if the one that fit the best is not big enough, it means no block is big enough!
-	SIZE_OF_DOUBLE_POINTER_BLOCK(blockSize, bestFit);
+	// if the one that fit the best is not big enough, it means no block is big enough! If it's not
+	// free, than there was no free block
+	const uint64_t blockSize = size_of_double_pointer_block(bestFit);
 
 	if(bestFit == NULL || blockSize < size ||
 	   (blockSize != size && blockSize < size + sizeof(BlockInformation) &&
 	    bestFit->nextBlock == NULL) ||
-	   bestFit->status.alloc_state != FREE) {
-#ifndef _ALLOCATOR_NOT_MT_SAVE
+	   bestFit->status != FREE) {
+#if !defined(_ALLOCATOR_NOT_MT_SAVE) && _PER_THREAD_ALLOCATOR != 1
 		result = pthread_mutex_unlock(&__my_malloc_globalObject.mutex);
 		checkResultForThreadErrorAndExit("INTERNAL: An Error occurred while trying to "
 		                                 "unlock the internal allocator mutex");
@@ -275,52 +189,46 @@ void* my_malloc(uint64_t size) {
 		return NULL;
 	};
 
-#if _PRINT_DEBUG == 1
-	printf("best Fit to requested size of %ld\n", size);
-	__my_malloc_debug_printSegment("best Fit: ", bestFit);
-#endif
-
 	if(blockSize == size) {
 		// block size and size needed for allocation is the same, only need to set the status to
 		// allocated
 
-		bestFit->status.alloc_state = ALLOCED;
-		bestFit->status.fill_state = FULLY_FILLED;
-	} else if(blockSize - size < (2u * sizeof(BlockInformation))) {
+		bestFit->status = ALLOCED;
+	} else if(blockSize - size <= (sizeof(BlockInformation))) {
 		// block size and size needed for allocation is nearly the same, but can't allocate a new
 		// block at the end, since it hasn't enough space for another BlockInformation, so only need
-		// to set the status to allocated, but some size is wasted, it can create a gap of 0 or
-		// more, to not do that, it has to be at least 2* the size, so that the block, that would be
-		// created isn#t 0 - Blocksize long, but at least BlockSize!
+		// to set the status to allocated, but some size is wasted, it can create a gap of 1 or
+		// more, that is fine, but gaps of 0 or less just "waste" that memory -- this handling
+		// implicates, that no position of previous or next block may be calculated by using the
+		// size!!
 
-		bestFit->status.alloc_state = ALLOCED;
-		bestFit->status.fill_state = NOT_FULLY_FILLED;
+		bestFit->status = ALLOCED;
 
 	} else {
 		BlockInformation* newBlock =
-		    (BlockInformation*)((pseudoByte*)bestFit + sizeof(BlockInformation) + size);
+		    (BlockInformation*)(((pseudoByte*)bestFit + sizeof(BlockInformation)) + size);
 		MEMCHECK_DEFINE_INTERNAL_USE(newBlock, sizeof(BlockInformation));
 
-		newBlock->status.alloc_state = FREE;
+		// the new gap is at least 1 byte big, see above!
+
+		newBlock->status = FREE;
 		newBlock->nextBlock = bestFit->nextBlock; // can be NULL
 		newBlock->previousBlock = bestFit;
 
-		bestFit->status.alloc_state = ALLOCED;
-		bestFit->status.fill_state = FULLY_FILLED;
-
+		bestFit->status = ALLOCED;
 		bestFit->nextBlock = newBlock;
-	}
 
-#if _PRINT_DEBUG == 1
-	__my_malloc_debug_printSegment("malloc: ", (BlockInformation*)__my_malloc_globalObject.data);
-#endif
+		if(newBlock->nextBlock != NULL) {
+			((BlockInformation*)newBlock->nextBlock)->previousBlock = newBlock;
+		}
+	}
 
 	void* returnValue = (pseudoByte*)bestFit + sizeof(BlockInformation);
 
 	MEMCHECK_DEFINE_INTERNAL_USE(bestFit, sizeof(BlockInformation));
 	VALGRIND_ALLOC(returnValue, size, 0, false);
 
-#ifndef _ALLOCATOR_NOT_MT_SAVE
+#if !defined(_ALLOCATOR_NOT_MT_SAVE) && _PER_THREAD_ALLOCATOR != 1
 	result = pthread_mutex_unlock(&__my_malloc_globalObject.mutex);
 	checkResultForThreadErrorAndExit(
 	    "INTERNAL: An Error occurred while trying to unlock the internal allocator mutex");
@@ -335,7 +243,8 @@ void my_free(void* ptr) {
 	if(ptr == NULL) {
 		return;
 	}
-#ifndef _ALLOCATOR_NOT_MT_SAVE
+
+#if !defined(_ALLOCATOR_NOT_MT_SAVE) && _PER_THREAD_ALLOCATOR != 1
 	int result = pthread_mutex_lock(&__my_malloc_globalObject.mutex);
 	// mutex errors are better when being asserted, since no real errors can occur, only when the
 	// system is already malfunctioning
@@ -345,42 +254,23 @@ void my_free(void* ptr) {
 
 	BlockInformation* information =
 	    (BlockInformation*)((pseudoByte*)ptr - sizeof(BlockInformation));
-#if _VALIDATE_BLOCKS == 1
-	if(!__my_malloc_isValidBlock(information)) {
-		printErrorAndExit("INTERNAL: you tried to free a invalid Block at address: %p\n", ptr);
-	}
-#endif
-#if !defined(_IGNORE_SECURITY_CHECKS)
 
-	if(information->status.alloc_state == FREE) {
+	if(information->status == FREE) {
 		printErrorAndExit("INTERNAL: you tried to free a already freed Block: %p\n", ptr);
 	}
-#endif
-	information->status.alloc_state = FREE;
+
+	information->status = FREE;
 	VALGRIND_FREE(ptr, 0);
 
 	BlockInformation* nextBlock = (BlockInformation*)information->nextBlock;
 	BlockInformation* previousBlock = (BlockInformation*)information->previousBlock;
 
-	// TODO: THIS is just here, to catch erro9rs, this should removed, once an error is fied, the
-	// error
-	// TODO: arises, when  running membench, it than corrupts some blocks!
-	if((pseudoByte*)previousBlock >
-	   (pseudoByte*)__my_malloc_globalObject.data + __my_malloc_globalObject.dataSize) {
-#ifndef _ALLOCATOR_NOT_MT_SAVE
-		int result = pthread_mutex_unlock(&__my_malloc_globalObject.mutex);
-		checkResultForThreadErrorAndExit(
-		    "INTERNAL: An Error occurred while trying to unlock the internal allocator mutex");
-#endif
-		return;
-	}
-
 	// merge with previous free block
-	if(previousBlock != NULL && previousBlock->status.alloc_state == FREE) {
+	if(previousBlock != NULL && previousBlock->status == FREE) {
 
 		// MERGE three free blocks into one: layout Previous | Current | Next => New Free one
-		if(nextBlock != NULL && nextBlock->status.alloc_state == FREE) {
-			previousBlock->nextBlock = nextBlock->nextBlock;
+		if(nextBlock != NULL && nextBlock->status == FREE) {
+			previousBlock->nextBlock = nextBlock->nextBlock; // Can be NULL
 
 			if(nextBlock->nextBlock != NULL) {
 				((BlockInformation*)nextBlock->nextBlock)->previousBlock = previousBlock;
@@ -397,7 +287,7 @@ void my_free(void* ptr) {
 		MEMCHECK_REMOVE_INTERNAL_USE(information, sizeof(BlockInformation));
 
 		// merge next free block with current one
-	} else if(nextBlock != NULL && nextBlock->status.alloc_state == FREE) {
+	} else if(nextBlock != NULL && nextBlock->status == FREE) {
 		information->nextBlock = nextBlock->nextBlock; // can be NULL
 
 		if(nextBlock->nextBlock != NULL) {
@@ -407,11 +297,7 @@ void my_free(void* ptr) {
 		MEMCHECK_REMOVE_INTERNAL_USE(nextBlock, sizeof(BlockInformation));
 	}
 
-#if _PRINT_DEBUG == 1
-	__my_malloc_debug_printSegment("free: ", (BlockInformation*)__my_malloc_globalObject.data);
-#endif
-
-#ifndef _ALLOCATOR_NOT_MT_SAVE
+#if !defined(_ALLOCATOR_NOT_MT_SAVE) && _PER_THREAD_ALLOCATOR != 1
 	result = pthread_mutex_unlock(&__my_malloc_globalObject.mutex);
 	checkResultForThreadErrorAndExit(
 	    "INTERNAL: An Error occurred while trying to unlock the internal allocator mutex");
@@ -449,7 +335,7 @@ void my_allocator_init(uint64_t size) {
 	}
 	// FREE is set with the 0 initialized region automatically (only here!)
 
-#ifndef _ALLOCATOR_NOT_MT_SAVE
+#if !defined(_ALLOCATOR_NOT_MT_SAVE) && _PER_THREAD_ALLOCATOR != 1
 	// initialize the mutex, use default as attr
 	int result = pthread_mutex_init(&__my_malloc_globalObject.mutex, NULL);
 	checkResultForThreadErrorAndExit("INTERNAL: An Error occurred while trying to initializing the "
@@ -464,13 +350,13 @@ void my_allocator_init(uint64_t size) {
 
 	firstBlock->nextBlock = NULL;
 	firstBlock->previousBlock = NULL;
-	firstBlock->status.alloc_state = FREE;
-	firstBlock->status.fill_state = FULLY_FILLED;
+	firstBlock->status = FREE;
 
-	int result2 = atexit(my_allocator_destroy);
-	checkForThreadError(result2,
-	                    "INTERNAL: An Error occurred while trying to register the atexit function",
-	                    exit(EXIT_FAILURE););
+	// TODO: investigate atexit with multiple threads
+	/* 	int result2 = atexit(my_allocator_destroy);
+	    checkForThreadError(result2,
+	                        "INTERNAL: An Error occurred while trying to register the atexit
+	   function", exit(EXIT_FAILURE);); */
 }
 
 // TODO: add realloc function
@@ -493,7 +379,7 @@ void my_allocator_destroy(void) {
 
 	__my_malloc_globalObject.data = NULL;
 
-#ifndef _ALLOCATOR_NOT_MT_SAVE
+#if !defined(_ALLOCATOR_NOT_MT_SAVE) && _PER_THREAD_ALLOCATOR != 1
 	result = pthread_mutex_destroy(&__my_malloc_globalObject.mutex);
 	checkResultForThreadErrorAndExit(
 	    "INTERNAL: An Error occurred while trying to destroy the internal mutex "
